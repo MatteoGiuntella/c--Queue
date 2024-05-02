48fc8e901bfe81ebc2a94e37504a4c6bc0359283
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-05-02 12:33:54 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+        // Inizializzo la queue con valori
+        Queue<int> queue = new Queue<int>(new int[] { 1, 2, 3, 4 });
+
+        // Rimuovo il primo elemento dalla coda
+        int primoNumeroRimosso = queue.Dequeue();
+        Console.WriteLine("Queue aggiornata:");
+        foreach (int numero in queue)
+        {
+            Console.WriteLine(numero);
+        }
